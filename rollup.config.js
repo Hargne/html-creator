d5fb948ecacddc25c26656ac45c92de41af7639e
Johan Hargne <johan.hargne@postkodlotteriet.se>
2018-02-26 08:37:40 +0100

    0.4.0 - Added build with RollupJS

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+/* eslint-disable import/no-extraneous-dependencies, no-console */
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const uglify = require('rollup-plugin-uglify');
+
+const externalLibraries = ['fs', 'path', 'lodash', 'mkdirp'];
+
+const regularBundleConfig = {
+	input: 'src/index.js',
+	output: {
+		file: 'dist/main.js',
+		format: 'cjs',
+	},
+	external: externalLibraries,
+	plugins: [
+		resolve(),
+		commonjs({
+			exclude: ['node_modules/**'],
+		}),
+	],
+};
+
+const minifiedBundleConfig = {
+	input: 'src/index.js',
+	output: {
+		file: 'dist/main.min.js',
+		format: 'cjs',
+	},
+	external: externalLibraries,
+	plugins: [
+		resolve(),
+		commonjs({
+			exclude: ['node_modules/**'],
+		}),
+		uglify(),
+	],
+};
+
+export default (process.env.BUILD === 'minified') ? minifiedBundleConfig : regularBundleConfig;
